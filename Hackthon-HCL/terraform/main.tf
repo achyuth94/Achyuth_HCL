cff1060974f066965ba85da272fb6baad0d5bbba
achyuth94 <41371307+achyuth94@users.noreply.github.com>
2025-11-22 14:27:15 +0530

    Update main.tf

--- Hackthon-HCL/terraform/main.tf
+++ Hackthon-HCL/terraform/main.tf
@@ -19,15 +19,6 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-# Log Analytics
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "${var.prefix}-law-${random_string.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
 # AKS
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
@@ -50,15 +41,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
       log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
     }
   }
-
-  depends_on = [azurerm_container_registry.acr]
 }
 
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
 
 # Key Vault
 data "azurerm_client_config" "current" {}
